421d3a9f64f9a283f79fa573917e15f952d723ec
Michal Šmahel <48548230+ceskyDJ@users.noreply.github.com>
2022-05-04 01:11:09 +0200

    Add diagrams representation entites

--- src/ClassDiagram.h
+++ src/ClassDiagram.h
@@ -9,9 +9,98 @@
 #ifndef ICP_PROJECT_CLASS_DIAGRAM_H
 #define ICP_PROJECT_CLASS_DIAGRAM_H
 
+#include "Class.h"
+#include "Relationship.h"
+
+/**
+ * Entity for complete class diagram
+ */
 class ClassDiagram
 {
+    /**
+     * Classes contained in class diagram
+     */
+    std::vector<Class> classes;
+    /**
+     * Relationships between classes
+     */
+    std::vector<Relationship> relationships;
 
+  public:
+    /**
+     * Constructor for empty class diagram
+     */
+    ClassDiagram(): classes{}, relationships{} {};
+    /**
+     * Constructor for initializing class diagram with known classes and relationships
+     *
+     * @param classes Classes
+     * @param relationships Relationships between classes
+     */
+    ClassDiagram(
+        std::vector<Class> classes,
+        std::vector<Relationship> relationships
+    ): classes{classes}, relationships{relationships} {};
+
+    /**
+     * Getter for classes
+     *
+     * @return Vector of stored classes
+     */
+    std::vector<Class> &getClasses()
+    {
+        return classes;
+    }
+
+    /**
+     * Setter for classes
+     *
+     * @param newClasses New vector of classes to store
+     */
+    void setClasses(std::vector<Class> &newClasses)
+    {
+        classes = newClasses;
+    }
+
+    /**
+     * Adds a new class to diagram
+     *
+     * @param newClass New class to add
+     */
+    void addClass(Class &newClass)
+    {
+        classes.push_back(newClass);
+    }
+
+    /**
+     * Getter for relationships between classes
+     *
+     * @return Vector of relationships
+     */
+    std::vector<Relationship> &getRelationships()
+    {
+        return relationships;
+    }
+
+    /**
+     * Adds a new relationship to diagram
+     *
+     * @param newRelationship New relationship to add
+     */
+    void addRelationship(Relationship &newRelationship)
+    {
+        relationships.push_back(newRelationship);
+    }
+
+    /**
+     * Setter for relationships between classes
+     *
+     * @param newRelationships New vector of relationships
+     */
+    void setRelationships(std::vector<Relationship> &newRelationships)
+    {
+        relationships = newRelationships;
+    }
 };
 
 #endif //ICP_PROJECT_CLASS_DIAGRAM_H
--- src/SequenceDiagram.h
+++ src/SequenceDiagram.h
@@ -9,9 +9,135 @@
 #ifndef ICP_PROJECT_SEQUENCE_DIAGRAM_H
 #define ICP_PROJECT_SEQUENCE_DIAGRAM_H
 
+#include "Actor.h"
+#include "Object.h"
+#include "Message.h"
+
+/**
+ * Entity for complete sequence diagram
+ */
 class SequenceDiagram
 {
+    /**
+     * Used actors
+     */
+    std::vector<Actor> actors;
+    /**
+     * Used objects
+     */
+    std::vector<Object> objects;
+    /**
+     * Contained messages
+     */
+    std::vector<Message> messages;
 
+  public:
+    /**
+     * Constructor for empty sequence diagram
+     */
+    SequenceDiagram(): actors{}, objects{}, messages{} {};
+    /**
+     * Constructor for initializing sequence diagram with known actors, objects and messages
+     *
+     * @param actors Actors to use
+     * @param objects Objects to use
+     * @param messages Messages to use
+     */
+    SequenceDiagram(
+        std::vector<Actor> actors,
+        std::vector<Object> objects,
+        std::vector<Message> messages
+    ): actors{actors}, objects{objects}, messages{messages} {};
+
+    /**
+     * Getter for used actors
+     *
+     * @return Vector of used actors
+     */
+    std::vector<Actor> &getActor()
+    {
+        return actors;
+    }
+
+    /**
+     * Setter for actors
+     *
+     * @param newActor New vector of actors to use
+     */
+    void setActor(std::vector<Actor> &newActors)
+    {
+        actors = newActors;
+    }
+
+    /**
+     * Adds a new actor to diagram
+     *
+     * @param actor New actor to add
+     */
+    void addActor(Actor &actor)
+    {
+        actors.push_back(actor);
+    }
+
+    /**
+     * Getter for used objects
+     *
+     * @return Vector of used objects
+     */
+    std::vector<Object> &getObjects()
+    {
+        return objects;
+    }
+
+    /**
+     * Setter for used objects
+     *
+     * @param newObjects New vector of objects to use
+     */
+    void setObjects(std::vector<Object> &newObjects)
+    {
+        objects = newObjects;
+    }
+
+    /**
+     * Adds a new object to diagram
+     *
+     * @param newObject New object to add
+     */
+    void addObject(Object &newObject)
+    {
+        objects.push_back(newObject);
+    }
+
+    /**
+     * Getter for used messages
+     *
+     * @return Vector of used messages
+     */
+    std::vector<Message> &getMessages()
+    {
+        return messages;
+    }
+
+    /**
+     * Setter for used messages
+     *
+     * @param newMessages New vector of messages to use
+     */
+    void setMessages(std::vector<Message> &newMessages)
+    {
+        messages = newMessages;
+    }
+
+    /**
+     * Adds a new message to diagram
+     *
+     * @param newMessage New message to add
+     */
+    void addMessage(Message &newMessage)
+    {
+        messages.push_back(newMessage);
+    }
 };
 
 #endif //ICP_PROJECT_SEQUENCE_DIAGRAM_H
