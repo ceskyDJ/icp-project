8918b1aec6feac49377858c255f22bbc16c5c933
ceskyDJ <ceskyDJ@users.noreply.github.com>
2022-05-06 09:49:08 +0000

    deploy: 3f4a3f14aeb8194776f1e7e7d9ba11562123b98e

--- search/classes_4.js
+++ search/classes_4.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['message_0',['Message',['../classMessage.html',1,'']]],
-  ['messagesender_1',['MessageSender',['../classMessageSender.html',1,'']]],
+  ['messagenode_1',['MessageNode',['../classMessageNode.html',1,'']]],
   ['messagetype_2',['MessageType',['../classMessageType.html',1,'']]],
   ['methodparameter_3',['MethodParameter',['../classMethodParameter.html',1,'']]]
 ];
--- search/functions_4.js
+++ search/functions_4.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['main_0',['main',['../main_8cpp.html#a0ddf1224851353fc92bfbff6f499fa97',1,'main.cpp']]],
-  ['message_1',['Message',['../classMessage.html#a00181227ad67cfb780619225414bd8ef',1,'Message::Message(std::string name, MessageType messageType, Actor *actor, Object *receivingObject)'],['../classMessage.html#adca89eb93e774f4dfd296a59f20df365',1,'Message::Message(std::string name, MessageType messageType, Object *sendingObject, Object *receivingObject)']]],
+  ['message_1',['Message',['../classMessage.html#a65de1b0d033555318f1c0a37d7db8737',1,'Message::Message(std::string name, MessageType messageType, Actor *sendingActor, Object *receivingObject, double sendingTime)'],['../classMessage.html#a7dd61fe2ff20f0d97aa8f485cf721149',1,'Message::Message(std::string name, MessageType messageType, Object *sendingObject, Object *receivingObject, double sendingTime)'],['../classMessage.html#af848a7e7e82e229dca22399fd05695d8',1,'Message::Message(std::string name, MessageType messageType, Object *sendingObject, Actor *receivingActor, double sendingTime)']]],
   ['messagetype_2',['MessageType',['../classMessageType.html#a7f3906b47395e8fa8f44c9a11a9a7d90',1,'MessageType::MessageType()=default'],['../classMessageType.html#ad1d0f969a07e35b16a794906700d1fcd',1,'MessageType::MessageType(Value value)']]],
   ['methodparameter_3',['MethodParameter',['../classMethodParameter.html#ac8860fe99bf291659e953e97fb851cf1',1,'MethodParameter']]]
 ];
